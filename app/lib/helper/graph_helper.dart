a4bba43e8aab2f0d829684026ab344c0fbbeee91
Florian Kunzweiler <kunzweiler@raceresult.com>
2020-07-17 14:54:18 +0200

    correct path

--- app/lib/helper/graph_helper.dart
+++ app/lib/helper/graph_helper.dart
@@ -4,7 +4,7 @@ import 'package:CommuneIsm/providers/auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
-const address = ":8000";
+const address = "http://192.168.99.100:8000";
 
 class GraphHelper {
   static Future<Map<dynamic, dynamic>> authPost(String body) async {
